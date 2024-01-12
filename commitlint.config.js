a51f0c80345a2381f63c813c3821e2f0be67c04c
sebanimm <gks32114221@gmail.com>
2024-01-12 19:46:18 +0900

    fix: commitlint 재수정

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,6 @@
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "subject-case": [
-      2,
-      "never",
-      ["sentence-case", "start-case", "pascal-case"],
-    ],
+    "subject-case": [2, "never", ["upper-case", "pascal-case"]],
   },
 };
