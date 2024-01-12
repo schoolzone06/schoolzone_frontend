fad54c655e527eafc66c8569b4153de50a495457
sebanimm <gks32114221@gmail.com>
2024-01-12 14:38:48 +0900

    fix: theme 객체로 묶어서 export 하도록 변경

--- src/styles/theme/theme.ts
+++ src/styles/theme/theme.ts
@@ -1,34 +1,27 @@
-const primary = "#2652F0";
-const secondary = "#3485FF";
-const tertiary = "#D0E3FF";
+const theme = {
+  primary: "#2652F0",
+  secondary: "#3485FF",
+  tertiary: "#D0E3FF",
 
-const error = "#F53232";
-const warn = "#FFEB39";
+  error: "#F53232",
+  warn: "#FFEB39",
 
-const black = "#000000";
-const white = "#FFFFFF";
+  black: "#000000",
+  white: "#FFFFFF",
 
-const gray = {
-  "50": "#FAFAFA",
-  "100": "#EDEDED",
-  "200": "#D4D4D4",
-  "300": "#BABABA",
-  "400": "#A1A1A1",
-  "500": "#878787",
-  "600": "#6E6E6E",
-  "700": "#545454",
-  "800": "#3B3B3B",
-  "900": "#212121",
-  "950": "#080808",
+  gray: {
+    "50": "#FAFAFA",
+    "100": "#EDEDED",
+    "200": "#D4D4D4",
+    "300": "#BABABA",
+    "400": "#A1A1A1",
+    "500": "#878787",
+    "600": "#6E6E6E",
+    "700": "#545454",
+    "800": "#3B3B3B",
+    "900": "#212121",
+    "950": "#080808",
+  },
 };
 
-export default {
-  primary,
-  secondary,
-  tertiary,
-  error,
-  warn,
-  black,
-  white,
-  gray,
-};
+export default theme;
