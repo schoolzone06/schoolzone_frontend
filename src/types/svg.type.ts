2d84bc2410718e3f36cc0c413731be5eab6a4ae8
lheesung <lheesung.dev@gmail.com>
2024-03-05 19:44:11 +0900

    feat: login page

---
+++ src/types/svg.type.ts
@@ -0,0 +1,7 @@
+export default interface SVGAttributeProps
+  extends React.SVGAttributes<HTMLOrSVGElement> {
+  width?: number;
+  height?: number;
+  color?: string;
+  // eslint-disable-next-line
+}
